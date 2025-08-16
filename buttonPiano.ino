3f173163fd4acf3ab09a86c888e8e4258b67ab88
nachiket-Arcot <arcotnachiket@gmail.com>
2025-08-16 17:30:11 -0400

    Update buttonPiano.ino

--- buttonPiano.ino
+++ buttonPiano.ino
@@ -30,7 +30,7 @@ void loop() {
     switchstate = digitalRead(buttonPins[i]); //Checks if button is pressed or not
     //If button is pressed will play corresponding note
     if (switchstate == HIGH){
-      tone(buzzerPin, notes[i], 200);
+      tone(buzzerPin, notes[i], 10);
       Serial.println(switchstate);
     }
   }
