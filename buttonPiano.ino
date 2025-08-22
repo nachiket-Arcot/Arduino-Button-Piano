b7eef9855cb417d59f9d00dfde7a1e395febc745
nachiket-Arcot <arcotnachiket@gmail.com>
2025-08-22 11:35:41 -0400

    Update buttonPiano.ino

--- buttonPiano.ino
+++ buttonPiano.ino
@@ -1,9 +1,11 @@
+#define BUTTON_NUM 7
+
 //Array of Pins for Buttons
-int buttonPins[7] = {
+int buttonPins[BUTTON_NUM] = {
   13, 12, 11, 10, 9, 8, 7
   };
 //Array for the Notes (Do-re-mi-fa-sol-la-si) frequency (in Hz)
-int notes[7] = {
+int notes[BUTTON_NUM] = {
   262, 294, 330, 349, 392, 440, 494
   };
 
@@ -15,7 +17,7 @@ void setup() {
   //Beginning Serial Connection
   Serial.begin(9600);
   //Setting up input (buttons)
-  for (int i = 0; i < 7; i++){
+  for (int i = 0; i < BUTTON_NUM; i++){
     pinMode(buttonPins[i], INPUT);
   }
   //Setting up output (buzzer)
@@ -26,7 +28,7 @@ void loop() {
   // put your main code here, to run repeatedly:
   int pitch = 0;
   //loops through notes and buttonPins array
-  for (int i = 0; i < 7; i++){
+  for (int i = 0; i < BUTTON_NUM; i++){
     switchstate = digitalRead(buttonPins[i]); //Checks if button is pressed or not
     //If button is pressed will play corresponding note
     if (switchstate == HIGH){
